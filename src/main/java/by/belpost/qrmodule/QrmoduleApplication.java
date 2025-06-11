0f906228c4a32bb2f5b7f302a7f5a2e9d75cb4c0
Andrey <adreygichan@gmail.com>
2025-06-11 14:29:33 +0300

    Initial commit

---
+++ src/main/java/by/belpost/qrmodule/QrmoduleApplication.java
@@ -0,0 +1,13 @@
+package by.belpost.qrmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/belpost/qrmodule/QrmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belpost.qrmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
